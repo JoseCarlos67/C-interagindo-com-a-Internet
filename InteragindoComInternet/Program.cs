9d02778260912b113b5069f6051f8df5cc18f69f
Carlos <carlos6767jose@gmail.com>
2022-08-26 20:29:18 -0300

    first commit

---
+++ InteragindoComInternet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteragindoComInternet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/");
+            requisicao.Method = "GET";
+
+        }
+    }
+}
